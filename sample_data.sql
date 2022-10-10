0e7b3e54b3375bb721867828142e58ff19b477e8
mansvaidya <96586402+mansvaidya@users.noreply.github.com>
2022-10-09 22:52:48 -0400

    Update sample_data.sql

--- sample_data.sql
+++ sample_data.sql
@@ -26,27 +26,27 @@ INSERT INTO `Stores` VALUES(10,'Raven Used Books',21,'Cambridge','MA','USA',8);
 --  POPULATE BOOKS
 -- --------------------------------------
 
-INSERT INTO `Books` VALUES('Phantoms in the Brain',1,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Addiction by Design',2,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Design of Everyday Things',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Emotional Design',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('The Guy Who Mistook His Wife for a Hat',4,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Lotus Feet and Almond Eyes',5,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('My Life in Full',6,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Perception',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Human Systems and Engineering',7,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('The Longevity Economy',8,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
+INSERT INTO `Books` VALUES(2,'Phantoms in the Brain',1,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',2);
+INSERT INTO `Books` VALUES(1,'Addiction by Design',2,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(5,'Design of Everyday Things',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(2,'Emotional Design',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(2,'The Guy Who Mistook His Wife for a Hat',4,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(6,'Lotus Feet and Almond Eyes',5,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',2);
+INSERT INTO `Books` VALUES(4,'My Life in Full',6,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(2,'Perception',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(1,'Human Systems and Engineering',7,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',100);
+INSERT INTO `Books` VALUES(5,'The Longevity Economy',8,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
 
-INSERT INTO `Books` VALUES('Grasp',9,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Microinteractions',10,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Encyclopedia of HCI',11,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('The Art of Finding Nemo',12,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('The Art of Finding Dory',13,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Living with Complexity',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('The Psychology of Everyday Things',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('User Centered System Design',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('The Design of Future Things',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
-INSERT INTO `Books` VALUES('Models of Human Memory',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA');
+INSERT INTO `Books` VALUES(7,'Grasp',9,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(1,'Microinteractions',10,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(1,'Encyclopedia of HCI',11,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(3,'The Art of Finding Nemo',12,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(3,'The Art of Finding Dory',13,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',2);
+INSERT INTO `Books` VALUES(5,'Living with Complexity',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(2,'The Psychology of Everyday Things',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(5,'User Centered System Design',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(5,'The Design of Future Things',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
+INSERT INTO `Books` VALUES(2,'Models of Human Memory',3,278,273849,'Yes',373,3484,3839,'Davolio','Lee','1948-12-08','Penguin Randomhouse','USA',1);
 
 -- --------------------------------------
 --  POPULATE GENRES
@@ -64,44 +64,44 @@ INSERT INTO `Genres` VALUES(7,'Other');
 --  POPULATE GENRE DETAILS
 -- --------------------------------------
 
-INSERT INTO `Genre Details` VALUES(1,3,2);
-INSERT INTO `Genre Details` VALUES(2,5,4);
-INSERT INTO `Genre Details` VALUES(3,2,1);
-INSERT INTO `Genre Details` VALUES(4,2,2);
-INSERT INTO `Genre Details` VALUES(5,3,2);
-INSERT INTO `Genre Details` VALUES(6,1,1);
+INSERT INTO `GenreDetails` VALUES(1,3,2);
+INSERT INTO `GenreDetails` VALUES(2,5,4);
+INSERT INTO `GenreDetails` VALUES(3,2,1);
+INSERT INTO `GenreDetails` VALUES(4,2,2);
+INSERT INTO `GenreDetails` VALUES(5,3,2);
+INSERT INTO `GenreDetails` VALUES(6,1,1);
 
 -- --------------------------------------
 --  POPULATE AUTHOR ORDERS
 -- --------------------------------------
 
-INSERT INTO `Author Orders` VALUES(1,'Sandra','Blakeslee',1,89);
-INSERT INTO `Author Orders` VALUES(2,'Natasha','Schull',1,378);
-INSERT INTO `Author Orders` VALUES(3,'Donald','Norman',8,27291);
-INSERT INTO `Author Orders` VALUES(4,'Oliver','Sacks',1,289);
-INSERT INTO `Author Orders` VALUES(5,'Shalini','Holkar',1,78);
-INSERT INTO `Author Orders` VALUES(6,'Indra','Nooyi',1,102);
-INSERT INTO `Author Orders` VALUES(7,'Alphonse','Chapanis',1,8290);
-INSERT INTO `Author Orders` VALUES(8,'Joseph','Coughlin',1,209);
-INSERT INTO `Author Orders` VALUES(9,'Sanjay','Sharma',1,28);
-INSERT INTO `Author Orders` VALUES(10,'Dan','Saffer',1,37);
-INSERT INTO `Author Orders` VALUES(11,'Claude','Ghaoui',1,223);
-INSERT INTO `Author Orders` VALUES(12,'Mark','Vaz',1,2719);
-INSERT INTO `Author Orders` VALUES(13,'John','Lasseter',1,1998);
+INSERT INTO `AuthorOrders` VALUES(1,'Sandra','Blakeslee',1,37);
+INSERT INTO `AuthorOrders` VALUES(2,'Natasha','Schull',1,378);
+INSERT INTO `AuthorOrders` VALUES(3,'Donald','Norman',8,27291);
+INSERT INTO `AuthorOrders` VALUES(4,'Oliver','Sacks',1,289);
+INSERT INTO `AuthorOrders` VALUES(5,'Shalini','Holkar',1,78);
+INSERT INTO `AuthorOrders` VALUES(6,'Indra','Nooyi',1,102);
+INSERT INTO `AuthorOrders` VALUES(7,'Alphonse','Chapanis',1,8290);
+INSERT INTO `AuthorOrders` VALUES(8,'Joseph','Coughlin',1,209);
+INSERT INTO `AuthorOrders` VALUES(9,'Sanjay','Sharma',1,28);
+INSERT INTO `AuthorOrders` VALUES(10,'Dan','Saffer',1,37);
+INSERT INTO `AuthorOrders` VALUES(11,'Claude','Ghaoui',1,223);
+INSERT INTO `AuthorOrders` VALUES(12,'Mark','Vaz',1,2719);
+INSERT INTO `AuthorOrders` VALUES(13,'John','Lasseter',1,1998);
 
 -- --------------------------------------
 --  POPULATE CUSTOMER ORDERS
 -- --------------------------------------
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
-INSERT INTO `Customer Orders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
+INSERT INTO `CustomerOrders` VALUES(1281273198371,2022-09-18,1,'Andy Roy','Yes',3,8,1);
 
